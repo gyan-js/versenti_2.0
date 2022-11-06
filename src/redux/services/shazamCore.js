a9466ceeb94b5bba32665f1cdd684207de981e7b
Gyan <gyan0304@gmail.com>
2022-11-06 01:33:55 -0500

    fix: Api key error 🐛

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set('X-RapidAPI-Key', '223933c0b0msh07b99fa2cf5b429p180527jsn361ea462c22b');
 
       return headers;
     },
