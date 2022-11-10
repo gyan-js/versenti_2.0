857976220eb3aea45393bed7ab7101d7622a6107
Gyan <gyan0304@gmail.com>
2022-11-10 01:00:10 -0500

    style: Linear gradients in app background 🎨

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ const App = () => {
 
   return (
     <div className="relative flex">
-      
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-lightblue">
-        <Searchbar />
-
+       <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br dark-bg  ">
+        
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
+            
               <Route path="/" element={<Discover />} />
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-charts" element={<TopCharts />} />
@@ -23,6 +23,7 @@ const App = () => {
               <Route path="/artists/:id" element={<ArtistDetails />} />
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
+        
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
