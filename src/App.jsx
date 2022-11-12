b1415ea7fdaf9f4bbe68561ece70965b106b7430
Gyan <gyan0304@gmail.com>
2022-11-12 04:39:48 -0500

    perf: switched theme integration to other file 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { current } from '@reduxjs/toolkit';
-import { createContext, useState } from 'react';
+
+import { createContext, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Switch from 'rc-switch'
 import ReactSwitch from 'react-switch'
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
+import { Searchbar, Sidebar, MusicPlayer, TopPlay, ThemeSwitch } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
 export const ThemeContext = createContext(null)
@@ -13,19 +13,17 @@ export const ThemeContext = createContext(null)
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
-  const [theme, setTheme] = useState("dark-bg")
-
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "dark-bg" ? "light-bg" : "dark-bg"))
-  }
-
+ 
+  
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
+   
     <div className="relative flex">
-       <Sidebar  />
-      <div className="flex-1 flex flex-col bg-gradient-to-br" id={theme}>
+       <Sidebar />
+       
+      <div className="flex-1 bg-[#343434] flex flex-col bg-gradient-to-br dark:bg-[#000] " >
         
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+      
+        <div className="px-6 h-[calc(100vh-60px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
             
@@ -44,7 +42,9 @@ const App = () => {
             
             <TopPlay />
           </div>
+          
         </div>
+        
       </div>
 
       {activeSong?.title && (
@@ -53,7 +53,7 @@ const App = () => {
         </div>
       )}
     </div>
-    </ThemeContext.Provider>
+   
   );
 };
 
